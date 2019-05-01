97332d827fe16c5a5abd37403ac47ed3590e5f1b
Seqian Wang <seqian@users.noreply.github.com>
2019-05-01 17:32:46 -0400

    Initial commit

---
+++ src/startingPoint.java
@@ -0,0 +1,29 @@
+public class
+
+public class startingPoint {
+
+    public static void main(String args[]) {
+
+    }
+
+//    frontier = PriorityQueue()
+//    frontier.put(start, 0)
+//    came_from = {}
+//    cost_so_far = {}
+//    came_from[start] = None
+//    cost_so_far[start] = 0
+//
+//            while not frontier.empty():
+//    current = frontier.get()
+//
+//            if current == goal:
+//            break
+//
+//            for next in graph.neighbors(current):
+//    new_cost = cost_so_far[current] + graph.cost(current, next)
+//            if next not in cost_so_far or new_cost < cost_so_far[next]:
+//    cost_so_far[next] = new_cost
+//            priority = new_cost + heuristic(goal, next)
+//         frontier.put(next, priority)
+//    came_from[next] = current
+}
